681ee26079196ba27907c0fd89b1356f225f4aa1
Bhargavi Prasad <bhargavi.prasad@rhibhus.com>
2024-11-27 09:25:24 +0530

    new api's

---
+++ Rhibhus.Reports.API/Rhibhus.Reports.API/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.AspNetCore.Reporting.WebDocumentViewer;
+using DevExpress.AspNetCore.Reporting.WebDocumentViewer.Native.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rhibhus.Reports.API.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("Admin")]
+    public class AdminController(IWebDocumentViewerMvcControllerService controllerService) : WebDocumentViewerController(controllerService)
+    {
+    }
+}
---
+++ Rhibhus.Reports.API/Rhibhus.Reports.API/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.AspNetCore.Reporting.WebDocumentViewer;
+using DevExpress.AspNetCore.Reporting.WebDocumentViewer.Native.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rhibhus.Reports.API.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("User")]
+    public class UserController(IWebDocumentViewerMvcControllerService controllerService) : WebDocumentViewerController(controllerService)
+    {
+    }
+}
