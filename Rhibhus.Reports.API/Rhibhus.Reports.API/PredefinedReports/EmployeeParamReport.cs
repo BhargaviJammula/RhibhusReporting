cb0517a9472279a6524acd6184c9df4761252561
Bhargavi Prasad <bhargavi.prasad@rhibhus.com>
2025-03-26 08:34:34 +0530

    LRK

--- Rhibhus.Reports.API/Rhibhus.Reports.API/PredefinedReports/EmployeeParamReport.cs
+++ Rhibhus.Reports.API/Rhibhus.Reports.API/PredefinedReports/EmployeeParamReport.cs
@@ -38,12 +38,64 @@ namespace Rhibhus.Reports.API.PredefinedReports
 
             var languageData = new LanguageService().ReadJsonFromFile(fileName);
 
-            this.FindControl("xrId", true).Text = languageData.GetValue("Id")?.ToString();
-            this.FindControl("xrName", true).Text = languageData.GetValue("Name")?.ToString();
-            this.FindControl("xrEmail", true).Text = languageData.GetValue("Email")?.ToString();
-            this.FindControl("xrAge", true).Text = languageData.GetValue("City")?.ToString();
-            this.FindControl("xrCity", true).Text = languageData.GetValue("Age")?.ToString();
+            #region Type 1
 
+            //this.FindControl("xrId", true).Text = languageData.GetValue("Id")?.ToString();
+            //this.FindControl("xrName", true).Text = languageData.GetValue("Name")?.ToString();
+            //this.FindControl("xrEmail", true).Text = languageData.GetValue("Email")?.ToString();
+            //this.FindControl("xrAge", true).Text = languageData.GetValue("City")?.ToString();
+            //this.FindControl("xrCity", true).Text = languageData.GetValue("Age")?.ToString();
+
+            #endregion
+
+            #region Type 2
+
+            //// List of control names in the report
+            //string[] controlNames = new string[] { "xrId", "xrName", "xrEmail", "xrAge", "xrCity" };
+
+            //// Corresponding keys for each control to retrieve from languageData
+            //string[] dataKeys = new string[] { "Id", "Name", "Email", "Age", "City" };
+
+            //// Loop through each control and set the text based on the corresponding language data
+            //for (int i = 0; i < controlNames.Length; i++)
+            //{
+            //    var control = this.FindControl(controlNames[i], true);
+            //    var value = languageData.GetValue(dataKeys[i])?.ToString();
+
+            //    if (control != null && value != null)
+            //    {
+            //        control.Text = value;
+            //    }
+            //}
+
+            #endregion
+
+            #region Type 3
+
+            // Iterate through each band in the collection
+            foreach (Band band in this.Bands.OfType<PageHeaderBand>())
+            {
+                // Iterate through all controls in the current band
+                foreach (XRTable control in band.Controls.OfType<XRTable>())
+                {
+                    var headerRow = control.Rows[0];
+
+                    // Iterate through the cells of the header row
+                    foreach (XRTableCell cell in headerRow.Cells)
+                    {
+                        // Retrieve the corresponding text from the language data
+                        var value = languageData.GetValue(cell.Text)?.ToString();
+
+                        if (!string.IsNullOrEmpty(value))
+                        {
+                            // Set the text of the header cell
+                            cell.Text = value;
+                        }
+                    }
+                }
+            }
+
+            #endregion
         }
     }
 }
